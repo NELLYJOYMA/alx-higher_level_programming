790dbdfbdf2374648e7eb30160d57a3020c75e1d
NELLYJOYMA <makena.nellijoy@gmail.com>
2024-01-03 06:46:39 -0800

    a class Square that defines a square by: (based on 6-square.py

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -1,1 +1,12 @@
-Write a class Square that defines a square by: (based on 6-square.py)
+#!/usr/bin/python3
+
+import sys
+
+
+def safe_function(fct, *args):
+    try:
+        result = fct(*args)
+        return (result)
+    except Exception as i:
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        return (None)
